0db16a26b499b62f1bb8ea941834a02ac6f13504
Rishabh Kumar <rishabhkumar@192.168.1.3>
2022-10-28 22:07:13 +0530

    Rowns And Columns

--- app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
+++ app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
@@ -3,15 +3,18 @@ package com.codewithrish.stevdzacompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codewithrish.stevdzacompose.ui.theme.StevdzaComposeTheme
 import com.codewithrish.stevdzacompose.ui.theme.Typography
 
@@ -22,12 +25,29 @@ class MainActivity : ComponentActivity() {
             StevdzaComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        Greeting("Android")
-                        CustomText(text = "CodeWithRish")
+//                    Column(
+//                        modifier = Modifier
+//                            .height(500.dp)
+//                            .width(500.dp)
+//                            .background(Color.LightGray),
+//                        horizontalAlignment = Alignment.CenterHorizontally
+//                    ) {
+//                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+//                        CustomItem(weight = 1f)
+//                    }
+
+                    Row(
+                        modifier = Modifier
+                            .height(500.dp)
+                            .width(500.dp)
+                            .background(Color.LightGray),
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+                        CustomItem(weight = 1f)
                     }
                 }
             }
@@ -36,25 +56,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
 
 @Composable
-fun CustomText(text: String ) {
-    Text(
-        text = text, 
-        style = Typography.h5
-    )
+fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+    ) {}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     StevdzaComposeTheme {
-        Column {
-            Greeting("Android")
-            CustomText(text = "CodeWithRish")
+//        Column(
+//            modifier = Modifier.height(500.dp),
+//            horizontalAlignment = Alignment.CenterHorizontally
+//        ) {
+//            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+//            CustomItem(weight = 1f)
+//        }
+
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
+            CustomItem(weight = 1f)
         }
+
     }
 }
