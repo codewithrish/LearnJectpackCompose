c45442f864e7dbd7cd457d0a9a650886b6f32031
Rishabh Kumar <rishabhkumar@192.168.1.4>
2022-10-29 16:53:48 +0530

    Text Fields

--- app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
+++ app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
@@ -2,18 +2,21 @@ package com.codewithrish.stevdzacompose
 
 import android.media.session.PlaybackState.CustomAction
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,6 +27,8 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -43,53 +48,64 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(MaterialTheme.colors.primary)
-                            .padding(24.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        ExpandableCard(
-                            title = "My Title",
-                            description = "Lorem Ipsum is simply dummy text of " +
-                                    "the printing and typesetting industry. Lorem Ipsum " +
-                                    "has been the industry's standard dummy text ever " +
-                                    "since the 1500s, when an unknown printer took a " +
-                                    "galley of type and scrambled it to make a type " +
-                                    "specimen book"
-                        )
-                    }
+                    Greeting()
                 }
             }
         }
     }
 }
 
-
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var text by remember {
+            mutableStateOf("Type here...")
+        }
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
+            },
+            //enabled = false
+            //readOnly = true,
+            label = {
+                Text(text = "Title")
+            },
+            // singleLine = true,
+            // maxLines = 2
+            leadingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Email, contentDescription = "Email Icon")
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = {
+                    Log.d("Trailing Icon", "Clicked")
+                }) {
+                    Icon(imageVector = Icons.Filled.Check, contentDescription = "Email Icon")
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    Log.d("ImeAction", "Clicked")
+                }
+            ),
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     StevdzaComposeTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.primary)
-                .padding(24.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            ExpandableCard(
-                title = "My Title",
-                description = "Lorem Ipsum is simply dummy text of " +
-                        "the printing and typesetting industry. Lorem Ipsum " +
-                        "has been the industry's standard dummy text ever " +
-                        "since the 1500s, when an unknown printer took a " +
-                        "galley of type and scrambled it to make a type " +
-                        "specimen book"
-            )
-        }
+        Greeting()
     }
 }
