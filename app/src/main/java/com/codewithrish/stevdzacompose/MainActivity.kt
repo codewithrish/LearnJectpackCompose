749e05119f891796e92913bf3842bdd765e5377b
Rishabh Kumar <rishabhkumar@192.168.1.4>
2022-10-29 18:05:50 +0530

    Coil Library

--- app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
+++ app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.BlurTransformation
+import coil.transform.CircleCropTransformation
+import coil.transform.GrayscaleTransformation
+import coil.transform.RoundedCornersTransformation
 import com.codewithrish.stevdzacompose.ui.theme.StevdzaComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,13 +36,7 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        GoogleButton(
-                            text = "Sign Up with Google",
-                            loadingText = "Creating Account...",
-                            onClicked = {
-                                Log.d("GoogleButton", "Clicked")
-                            }
-                        )
+                        CoilImage()
                     }
                 }
             }
@@ -41,6 +44,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+fun CoilImage() {
+    Box(
+        modifier = Modifier
+            .width(250.dp)
+            .height(250.dp),
+        contentAlignment = Alignment.Center
+    ){
+        val painter = rememberImagePainter(
+            data = "https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?cs=srgb&dl=pexels-mike-b-170811.jpg&fm=jpg",
+            builder = {
+                placeholder(R.drawable.placeholder)
+                error(R.drawable.ic_error)
+                crossfade(1000)
+                transformations(
+                    GrayscaleTransformation(),
+                    // CircleCropTransformation(),
+                    BlurTransformation(LocalContext.current),
+                    RoundedCornersTransformation(50f)
+                )
+            }
+        )
+        val painterState = painter.state
+        Image(painter = painter, contentDescription = "Car Image")
+
+//        if (painterState is ImagePainter.State.Loading) {
+//            CircularProgressIndicator()
+//        }
+
+        // Text(text = "Rishabh", color = Color.White, style = MaterialTheme.typography.h1)
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -51,11 +87,7 @@ fun DefaultPreview() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            GoogleButton(
-                text = "Sign Up with Google",
-                loadingText = "Creating Account...",
-                onClicked = {}
-            )
+            CoilImage()
         }
     }
 }
