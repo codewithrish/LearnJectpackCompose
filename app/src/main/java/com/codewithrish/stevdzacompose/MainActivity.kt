c46b562ac2f67260b9c1e387ca2f24eb9db2e95a
Rishabh Kumar <rishabhkumar@192.168.1.3>
2022-10-29 14:38:57 +0530

    Box Layout

--- app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
+++ app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -15,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.codewithrish.stevdzacompose.ui.theme.StevdzaComposeTheme
 import com.codewithrish.stevdzacompose.ui.theme.Typography
 
@@ -27,28 +30,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-//                    Column(
-//                        modifier = Modifier
-//                            .height(500.dp)
-//                            .width(500.dp)
-//                            .background(Color.LightGray),
-//                        horizontalAlignment = Alignment.CenterHorizontally
-//                    ) {
-//                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
-//                        CustomItem(weight = 1f)
-//                    }
-
-                    Row(
-                        modifier = Modifier
-                            .height(500.dp)
-                            .width(500.dp)
-                            .background(Color.LightGray),
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
-                        CustomItem(weight = 1f)
-                    }
+                    Greeting()
                 }
             }
         }
@@ -56,46 +38,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ColumnScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
-    Surface(
-        modifier = Modifier
-            .width(200.dp)
-            .weight(weight),
-        color = color
-    ) {}
-}
+fun Greeting() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Blue),
+//                .width(100.dp)
+//                .height(100.dp)
+//                .verticalScroll(rememberScrollState())
+        contentAlignment = Alignment.Center
 
-@Composable
-fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colors.primary) {
-    Surface(
-        modifier = Modifier
-            .width(50.dp)
-            .height(50.dp)
-            .weight(weight),
-        color = color
-    ) {}
+        ) {
+            Box(modifier = Modifier.width(50.dp).height(50.dp).background(Color.Green))
+            Text(text = "I Love Android", fontSize = 40.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     StevdzaComposeTheme {
-//        Column(
-//            modifier = Modifier.height(500.dp),
-//            horizontalAlignment = Alignment.CenterHorizontally
-//        ) {
-//            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
-//            CustomItem(weight = 1f)
-//        }
-
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            CustomItem(weight = 3f, color = MaterialTheme.colors.secondary)
-            CustomItem(weight = 1f)
-        }
-
+        Greeting()
     }
 }
