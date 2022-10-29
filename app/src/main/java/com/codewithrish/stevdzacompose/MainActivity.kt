e4aac70424060d8a91b54ad8b28cc80140f77b26
Rishabh Kumar <rishabhkumar@192.168.1.4>
2022-10-29 17:40:29 +0530

    Google Button Design

--- app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
+++ app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
@@ -1,44 +1,18 @@
 package com.codewithrish.stevdzacompose
 
-import android.media.session.PlaybackState.CustomAction
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.text.selection.DisableSelection
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ParagraphStyle
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.codewithrish.stevdzacompose.ui.theme.StevdzaComposeTheme
-import com.codewithrish.stevdzacompose.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,64 +22,40 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting()
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        GoogleButton(
+                            text = "Sign Up with Google",
+                            loadingText = "Creating Account...",
+                            onClicked = {
+                                Log.d("GoogleButton", "Clicked")
+                            }
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        var text by remember {
-            mutableStateOf("Type here...")
-        }
-        OutlinedTextField(
-            value = text,
-            onValueChange = { newText ->
-                text = newText
-            },
-            //enabled = false
-            //readOnly = true,
-            label = {
-                Text(text = "Title")
-            },
-            // singleLine = true,
-            // maxLines = 2
-            leadingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Filled.Email, contentDescription = "Email Icon")
-                }
-            },
-            trailingIcon = {
-                IconButton(onClick = {
-                    Log.d("Trailing Icon", "Clicked")
-                }) {
-                    Icon(imageVector = Icons.Filled.Check, contentDescription = "Email Icon")
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Search
-            ),
-            keyboardActions = KeyboardActions(
-                onSearch = {
-                    Log.d("ImeAction", "Clicked")
-                }
-            ),
-        )
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     StevdzaComposeTheme {
-        Greeting()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            GoogleButton(
+                text = "Sign Up with Google",
+                loadingText = "Creating Account...",
+                onClicked = {}
+            )
+        }
     }
 }
