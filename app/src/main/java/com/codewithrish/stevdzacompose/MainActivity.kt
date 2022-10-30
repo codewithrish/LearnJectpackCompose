7dfd9a10e2224517a9d5bae0fd4dbea0405c6bf4
Rishabh Kumar <rishabhkumar@192.168.1.4>
2022-10-30 15:42:06 +0530

    Two Screens With Simple Navigation

--- app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
+++ app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
@@ -3,42 +3,21 @@ package com.codewithrish.stevdzacompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.codewithrish.stevdzacompose.ui.theme.StevdzaComposeTheme
 
 
 class MainActivity: ComponentActivity() {
+    
+    lateinit var navController: NavHostController
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StevdzaComposeTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    var value by remember {
-                        mutableStateOf(0)
-                    }
-                    ArcProgress(
-                        indicatorValue = value
-                    )
-                    TextField(
-                        value = value.toString(),
-                        onValueChange = {
-                            value = if (it.isNotEmpty()) it.toInt() else 0
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Number
-                        )
-                    )
-                }
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
