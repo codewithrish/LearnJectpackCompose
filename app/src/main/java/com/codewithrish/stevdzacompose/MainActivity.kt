eaa646d5491fa6153f801463e0884c9a780d0ce6
Rishabh Kumar <rishabhkumar@192.168.1.4>
2022-10-30 15:10:16 +0530

    Animated Progress bar

--- app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
+++ app/src/main/java/com/codewithrish/stevdzacompose/MainActivity.kt
@@ -3,14 +3,14 @@ package com.codewithrish.stevdzacompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import com.codewithrish.stevdzacompose.ui.theme.StevdzaComposeTheme
 
 
@@ -19,39 +19,27 @@ class MainActivity: ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StevdzaComposeTheme {
-                MyComposable()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    var value by remember {
+                        mutableStateOf(0)
+                    }
+                    ArcProgress(
+                        indicatorValue = value
+                    )
+                    TextField(
+                        value = value.toString(),
+                        onValueChange = {
+                            value = if (it.isNotEmpty()) it.toInt() else 0
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
+                }
             }
         }
-    }
-}
-
-@Composable
-fun MyComposable() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        var name by remember {
-            mutableStateOf("")
-        }
-
-        val maxChar = 10
-
-        OutlinedTextField(
-            value = name,
-            onValueChange = {
-                if (it.length <= maxChar) {
-                    name = it
-                }
-            },
-            label = {
-                Text(text = "Name")
-            },
-            placeholder = {
-                Text(text = "Enter your name")
-            },
-            maxLines = 1
-        )
     }
 }
